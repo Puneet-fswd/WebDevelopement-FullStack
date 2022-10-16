9e62ecaa68bffb5970b0b792e0365a4670cdcfbc
Puneet-fswd <puneetamrutkar@gmail.com>
2022-10-16 23:12:33 +0530

    init

--- Theory and Practice/NodeJs/custom_modules/index.js
+++
@@ -1,3 +0,0 @@
-const mod = require('./module.js');
-console.log(mod.sum([0, 3, 4, 5, 76, 23, 12, 100]));
-console.log(mod.avg([1, 2, 3, 4, 5, 6, 7, 8, 9]));
--- Theory and Practice/NodeJs/nodejs_modules.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let text = fs.readFileSync("text.txt", "utf-8");      //fs is nodejs module
-console.log("The content of text.txt is: ");
-console.log(text);
-text = text.replace("text","txt");      //contents replaced in backend
-console.log(text);
-
-// fs.writeFileSync("txt.txt", text); --> this creates a new file txt.txt with the contents of text.txt
